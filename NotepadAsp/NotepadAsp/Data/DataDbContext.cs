81fed8fbc31f50b4360631ad5ba3952ab7f951eb
Alice <romankoalice@gmail.com>
2024-05-25 00:43:10 +0300

    last2

--- NotepadAsp/NotepadAsp/Data/DataDbContext.cs
+++ NotepadAsp/NotepadAsp/Data/DataDbContext.cs
@@ -9,6 +9,7 @@ namespace NotepadAsp.Data
     public class DataDbContext : DbContext
     {
 
+
         public DbSet<User> Users { get; set; }
         public DbSet<Note> Notes { get; set; }
         public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) 
