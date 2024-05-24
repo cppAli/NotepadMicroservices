4ba3e90a3a4618f2adfbaa28d77499f62fe93453
Alice <romankoalice@gmail.com>
2024-05-25 00:18:47 +0300

    add struct pj, models base table

---
+++ NotepadAsp/NotepadAsp/Models/Note.cs
@@ -0,0 +1,30 @@
+﻿using NotesApp.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotepadAsp.Models
+{
+    [Table("note")]
+    public class Note
+    {
+        [Key]
+        [Column("note_id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("title")]
+        public string Title { get; set; }
+
+        [Required]
+        [Column("content")]
+        public string Content { get; set; }
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("User")]
+        [Column("user_id")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ NotepadAsp/NotepadAsp/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using NotepadAsp.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NotesApp.Models
+{
+    [Table("user")]
+    public class User
+    {
+        [Key]
+        [Column("user_id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("username")]
+        public string Username { get; set; }
+
+        [Required]
+        [Column("password")]
+        public string Password { get; set; }
+
+        public ICollection<Note> Notes { get; set; }
+    }
+}
