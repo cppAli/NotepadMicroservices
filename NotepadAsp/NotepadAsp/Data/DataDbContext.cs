59f5fc50df91d17ace1e919e4aa60e74d8bff139
Alice <romankoalice@gmail.com>
2024-05-25 00:36:36 +0300

    add database setting

---
+++ NotepadAsp/NotepadAsp/Data/DataDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using NotepadAsp.Models;
+using NotesApp.Models;
+
+namespace NotepadAsp.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DataDbContext(DbContextOptions<DataDbContext> options) : base(options) 
+        {
+            try
+            {
+                var databaseCreator = Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator;
+                if(databaseCreator != null)
+                {
+                    //Create db if cannot connect
+                    if (!databaseCreator.CanConnect()) databaseCreator.Create();
+
+                    //Create table if no tables exits
+                    if(!databaseCreator.HasTables()) databaseCreator.CreateTables();
+                }
+            } 
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        
+        }
+    }
+}
